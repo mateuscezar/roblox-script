9f5bcfc94ce095e6f61b2e79d0ced38022844881
mateuscezar <mateuscezarf@gmail.com>
2023-01-10 14:53:25 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,5 +5,7 @@ local games = {
 local supported = games[game.PlaceId]
 
 if supported then
+    print("carregando... " .. supported)
+    print("https://raw.githubusercontent.com/mateuscezar/roblox-script/main/games/" .. supported .. ".lua")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/mateuscezar/roblox-script/main/games/" .. supported .. ".lua"))()
 end
